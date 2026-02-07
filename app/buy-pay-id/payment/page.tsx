d7a4a6fc6b79ea25c62844697862d8ccec171cdd
justinkanet-tech <justinkanet@gmail.com>
2026-02-07 08:52:56 +0100

    Update page.tsx

--- app/buy-pay-id/payment/page.tsx
+++ app/buy-pay-id/payment/page.tsx
@@ -30,7 +30,7 @@ export default function PaymentPage() {
   }
 
   const handleCopyAccountNumber = () => {
-    navigator.clipboard.writeText("6598432190")
+    navigator.clipboard.writeText("9020336068")
     setCopiedAccount(true)
     setTimeout(() => setCopiedAccount(false), 2000)
   }
@@ -89,7 +89,7 @@ export default function PaymentPage() {
             <div>
               <p className="text-gray-700 mb-1">Account Number</p>
               <div className="flex items-center justify-between">
-                <p className="font-bold">6598432190</p>
+                <p className="font-bold">9020336068</p>
                 <button
                   onClick={handleCopyAccountNumber}
                   className="bg-orange-400 text-white px-3 py-1 rounded text-sm"
@@ -101,12 +101,12 @@ export default function PaymentPage() {
 
             <div>
               <p className="text-gray-700 mb-1">Bank Name</p>
-              <p className="font-bold">Moniepoint-mfb</p>
+              <p className="font-bold">Smart-cash</p>
             </div>
 
             <div>
               <p className="text-gray-700 mb-1">Account Name</p>
-              <p className="font-bold">IMAMU-IMAMU</p>
+              <p className="font-bold">Michael-adewumi</p>
             </div>
           </div>
 
